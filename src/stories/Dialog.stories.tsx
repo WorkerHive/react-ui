eb1ad73667a86ef447d32c46d0686d1db4fb1ae4
Ross Leitch <professional.balbatross@gmail.com>
2021-01-09 16:20:57 +1300

    Storybook build

--- src/stories/Dialog.stories.tsx
+++ src/stories/Dialog.stories.tsx
@@ -25,19 +25,3 @@ Primary.args = {
   open: true
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
--- src/stories/List.stories.tsx
+++ src/stories/List.stories.tsx
@@ -21,20 +21,3 @@ Primary.args = {
   type: {name: 'String'},
   data:[{name: 'Ross'}]
 };
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
