209aa6174b0a500faa30f7a01df8a3ebe13f4eb1
Ross Leitch <professional.balbatross@gmail.com>
2021-01-10 21:25:26 +1300

    Fix reference issue

--- src/components/kanban/index.tsx
+++ src/components/kanban/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import moment from 'moment';
 import Board from '@lourenci/react-kanban'
-import { TeamCircles } from '../team-circles';
+import { TeamCircles } from '../..';
 
 //import '@lourenci/react-kanban/dist/styles.css'
 
