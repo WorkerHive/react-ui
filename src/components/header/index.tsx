ea12c9173ba131bac413251bbc6ce9378fd9532d
Ross Leitch <professional.balbatross@gmail.com>
2021-01-11 18:06:09 +1300

    header styling

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -60,7 +60,6 @@ export const Header : React.FC<HeaderProps> = ({
 
 export const StyledHeader = styled(Header)`
   display: flex;
-  flex: 1;
   height: 50px;
   justify-content:space-between;
   padding-left: 12px;
